098dff7d7168b85f7987be1cff8c3b184d01d2c0
Isaac Gray <44359277+I-Gray@users.noreply.github.com>
2023-07-18 13:27:21 +0200

    Update dotfiles.sh

--- dotfiles.sh
+++ dotfiles.sh
@@ -1,15 +1,5 @@
 #!/bin/sh
 
-echo "installing Oh-My-ZSH..."
-curl -L http://install.ohmyz.sh | sh
-
-echo "zsh plugin setup..."
-cd ~/.oh-my-zsh/custom/plugins
-git clone git://github.com/zsh-users/zsh-syntax-highlighting.git
-
-echo "set ZSH as shell..."
-chsh -s /bin/zsh
-
 echo "fetching dot files from Github"
 cd ~
 git clone git@github.com:I-Gray/dotfiles.git dotfiles
